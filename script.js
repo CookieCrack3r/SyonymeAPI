49c847695dd4375f54a9357164b215e3c0277921
cookie <mail@vks.info>
2023-07-28 14:19:42 +0200

    add pokemon api

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 
-
-
-
-
-
-
-
 async function getSynonyms(){
 
-    let url = 'https://www.openthesaurus.de/synonyme/​search?q=test&format=application/json';
+    let url = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
     let response = await fetch(url);
     let responseAsJson = await response.json();
+
+    let results = responseAsJson['results'];
     console.log('response is', responseAsJson);
+    renderSynsets(results)
+}
+
+function renderSynsets(results){
+    let container = document.getElementById('container');
+    container.innerHTML = `<div> Es wurden <b> ${results.length} </b> Synonyme geladen. </div>`;
+
+    for (let index = 0; index < results.length; index++) {
+        const element = results[index];
+
+        container.innerHTML += `${element['name']}<br>`;
+    }
+
 
 }
